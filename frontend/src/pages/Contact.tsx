c1c741c91142644ba418a8f157f2f86b7d24f614
fabrizio.lombardi <fabrizio.lombardi@smc.it>
2026-06-02 21:50:20 +0200

    Correct CV URL

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -64,7 +64,7 @@ export default function Contact() {
 
           <hr />
 
-          <Button className="w-full" href="/cv" newTab>
+          <Button className="w-full" href="/api/cv" newTab>
             <DownloadIcon />
             Scarica CV (PDF)
           </Button>
